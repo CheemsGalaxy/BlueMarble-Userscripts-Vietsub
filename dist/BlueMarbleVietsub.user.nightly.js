5db849e59ec9427e0d7195e2cc87677774f1c4e2
CheemsGalaxy <149580222+CheemsGalaxy@users.noreply.github.com>
2026-08-09 23:27:31 +0700

    Update BlueMarbleVietsub.user.nightly.js

--- dist/BlueMarbleVietsub.user.nightly.js
+++ dist/BlueMarbleVietsub.user.nightly.js
@@ -1,16 +1,17 @@
 // ==UserScript==
-// @name         Blue Marble
+// @name         Blue Marble (Nightly)
 // @namespace    https://github.com/SwingTheVine/
-// @version      0.87.17
+// @version      0.87.17.n
 // @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
 // @author       SwingTheVine
 // @author       TWY
+// @author       CheemsGalaxy (Vietsub)
 // @license      MPL-2.0
 // @supportURL   https://discord.gg/tpeBPy46hf
 // @homepageURL  https://bluemarble.lol/
 // @icon         https://raw.githubusercontent.com/CheemsGalaxy/BlueMarble-Userscripts-Vietsub/main/dist/assets/Favicon.png
-// @updateURL    https://raw.githubusercontent.com/CheemsGalaxy/BlueMarble-Userscripts-Vietsub/main/dist/BlueMarbleVietsub.user.js
-// @downloadURL  https://raw.githubusercontent.com/CheemsGalaxy/BlueMarble-Userscripts-Vietsub/main/dist/BlueMarbleVietsub.user.js
+// @updateURL    https://raw.githubusercontent.com/CheemsGalaxy/BlueMarble-Userscripts-Vietsub/main/dist/BlueMarbleVietsub.user.nightly.js
+// @downloadURL  https://raw.githubusercontent.com/CheemsGalaxy/BlueMarble-Userscripts-Vietsub/main/dist/BlueMarbleVietsub.user.nightly.js
 // @match        https://wplace.live/*
 // @run-at       document-start
 // @grant        GM.addStyle
